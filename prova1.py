efe1b8bbe37c809ef2da65efd827d82958d6b8c6
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-09-28 21:46:27 -0300

    Add files via upload
    Lógica de Programação Aula 1 - prova

---
+++ prova1.py
@@ -0,0 +1,10 @@
+nome = "Suzana"
+print(nome)
+idade = 33
+print(idade)
+peso = 56.7
+print(peso)
+nome = input("Digite seu nome ")
+idade = int(input("Quantos anos você tem? "))
+peso = float(input("Qual é o seu peso? "))
+print(f"Olá {nome}! Você tem {idade} anos e você pesa {peso} kilos.")
